66ef98625c50f5dff383d046294ab50b6b5af8e9
vaishnavi2504 <vaishnavibhat26@gmail.com>
2017-01-18 12:21:20 -0600

    Update ThirdMaximum.java

--- Easy/ThirdMaximum.java
+++ Easy/ThirdMaximum.java
@@ -1,13 +1,11 @@
 //https://leetcode.com/problems/third-maximum-number/
 
 public class Solution {
-    public int thirdMax(int[] arr) {
-        Arrays.sort(arr);
+    public int thirdMax(int[] arr) {        
        Map<Integer,Integer> tm = new TreeMap<>();
        for(int x:arr){
            if(!tm.containsKey(x))tm.put(x,1);
-       }
-       System.out.println(tm);
+       }       
        if(tm.size()==1)return (int)tm.keySet().toArray()[0];
        if(tm.size()==2)return (int)tm.keySet().toArray()[1];
        else return (int)tm.keySet().toArray()[tm.size()-3];
