8d10ec58a91bdf5cb1f95b3847b1b31b5e2f04c4
vaishnavi2504 <vaishnavibhat26@gmail.com>
2017-01-26 16:47:52 -0600

    Create Number Complement.java

---
+++ Easy/Number Complement.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/number-complement/
+
+
+public class Solution {
+    
+    public int binToint(String s){
+        int res=0;
+        for(int i=0;i<s.length();i++){
+            int c=(int)s.charAt(i)-'0';
+            res+=c*(int)Math.pow(2,s.length()-1-i);
+        }
+        return res;
+    }
+    
+    
+    public int findComplement(int num) {
+        StringBuilder sb=new StringBuilder();
+        int count=0;
+        while(num!=0){
+            count=num&1;
+            if(count==1)sb.append("0");
+            else sb.append("1");
+            num=num>>>1;
+        }
+        return new String(sb.reverse()));
+        
+    }
+}
