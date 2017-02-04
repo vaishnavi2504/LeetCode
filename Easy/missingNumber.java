8cb0d2120972448a59c6cbf861e635bee04d3ae0
vaishnavi2504 <vaishnavibhat26@gmail.com>
2017-02-04 13:20:07 -0600

    Update missingNumber.java

--- Easy/missingNumber.java
+++ Easy/missingNumber.java
@@ -1,6 +1,7 @@
 //https://leetcode.com/problems/missing-number/
-
+// 2 methods
 public class Solution {
+    //Not linnear because of Arrays.sort
     public int missingNumber(int[] nums) {
         int n=nums.length;
         Arrays.sort(nums);
@@ -12,5 +13,15 @@ public class Solution {
             if(nums[i]-nums[i-1]!=1)return (nums[i]+nums[i-1])/2;
         }
         return -1;
+    }
+    
+    //Method2 , no sort. linnear time
+    public int missingNumber(int[] a) {
+        int n=a.length;
+        int sum=((n)*(n+1))/2;
+        for(int i=0;i<a.length;i++){
+            sum-=a[i];
+        }
+        return sum;
     }
 }
