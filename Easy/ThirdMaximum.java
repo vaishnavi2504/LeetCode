908c375eb4e5deb2f895437569a3723cc2a124d1
vaishnavi2504 <vaishnavibhat26@gmail.com>
2017-02-04 08:52:56 -0600

    Update ThirdMaximum.java

--- Easy/ThirdMaximum.java
+++ Easy/ThirdMaximum.java
@@ -1,5 +1,7 @@
 //https://leetcode.com/problems/third-maximum-number/
+// There are 2 approaches
 
+//Method1 using hash map
 public class Solution {
     public int thirdMax(int[] arr) {        
        Map<Integer,Integer> tm = new TreeMap<>();
@@ -12,3 +14,31 @@ public class Solution {
        
     }
 }
+
+//method 2 using arrays// Time complx= 3*(O(n))
+public int thirdMax(int[] arr) {
+       int max1=Integer.MIN_VALUE,max2=Integer.MIN_VALUE,max3=Integer.MIN_VALUE,flag=0;
+       
+       //fIRST MAX
+       for(int x:arr){
+           if(x>max1)max1=x;
+       }
+       
+       //Second max
+       for(int x:arr){
+           if(x>max2&&max1!=x)max2=x;
+       }
+       
+       //third max
+       for(int x:arr){
+           if(x>=max3&&max1!=x&&max2!=x){
+               max3=x;
+               if(x==Integer.MIN_VALUE)flag=1;
+           }
+       }
+       
+       if(max3!=Integer.MIN_VALUE||flag==1)return max3;
+       
+       else return max1;
+       
+}     
