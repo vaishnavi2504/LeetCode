ee2c7baff143ae67a4e357711b6abc437661f518
vaishnavi2504 <vaishnavibhat26@gmail.com>
2017-02-20 22:40:36 -0600

    Update ValidPerfectSquare.java

--- Easy/ValidPerfectSquare.java
+++ Easy/ValidPerfectSquare.java
@@ -1,9 +1,23 @@
 //https://leetcode.com/problems/valid-perfect-square/
 
 public class Solution {
-    public boolean isPerfectSquare(int num) {
-        
-        int x=(int)Math.sqrt(num);
+    
+    public int mySqrt(int n){
+        //Use binary search
+        int l=1,h=x,mid=0;
+        while(l<=h){
+            mid=(l+h)/2;
+            long chk=mid*mid;
+            if(chk==n)return mid;
+            else if(chk>mid){
+                l=mid-1;
+            }else{
+                h=mid+1;
+            }
+        }
+    }
+    public boolean isPerfectSquare(int num) {        
+        int x=mysqrt(num);
         if(x*x==num)return true;
         else return false;
     }
