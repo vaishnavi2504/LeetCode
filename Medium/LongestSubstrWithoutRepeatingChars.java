91aa74f8c8bda72328c9968fd2e757a3e9f35b24
vaishnavi2504 <vaishnavibhat26@gmail.com>
2017-03-27 06:23:25 -0500

    Update LongestSubstrWithoutRepeatingChars.java

--- Medium/LongestSubstrWithoutRepeatingChars.java
+++ Medium/LongestSubstrWithoutRepeatingChars.java
@@ -19,22 +19,22 @@ public class Solution {
     }
 }
 
-//Approach2 using hashset
+//Approach2 Using hashet
 
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-       HashMap<Character,Integer> hm=new HashMap<>();
-        int i=0,j=0,maxLen=0;
-        for(;i<s.length();++i){
-            char c=s.charAt(i);
-            if(hm.containsKey(c)){
-                //j points to the beginning of the non repeating characters sub string
-                //To handle cases like "abba"
-                j=Math.max(j,hm.get(c)+1);
-            }
-            hm.put(c,i);
-            maxLen=Math.max(maxLen,-j+i+1);
-        }
-        return maxLen;
+       Set<Character> st=new HashSet<>();
+       int i=0,j=0,max=0;
+       while(j<s.length()){
+           char c=s.charAt(j);
+           if(!st.contains(c)){
+               st.add(c);
+               j++;
+               max=Math.max(max,st.size());
+           }else{
+               st.remove(s.charAt(i++));
+           }
+       }
+       return max;
     }
 }
