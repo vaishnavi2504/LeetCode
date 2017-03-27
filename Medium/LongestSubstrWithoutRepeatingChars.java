71650e090d7594831c4647af9af424fda3fbb1f4
vaishnavi2504 <vaishnavibhat26@gmail.com>
2017-03-27 06:22:15 -0500

    Update LongestSubstrWithoutRepeatingChars.java

--- Medium/LongestSubstrWithoutRepeatingChars.java
+++ Medium/LongestSubstrWithoutRepeatingChars.java
@@ -1,5 +1,26 @@
 //https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
+//Approach1
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+       HashMap<Character,Integer> hm=new HashMap<>();
+        int i=0,j=0,maxLen=0;
+        for(;i<s.length();++i){
+            char c=s.charAt(i);
+            if(hm.containsKey(c)){
+                //j points to the beginning of the non repeating characters sub string
+                //To handle cases like "abba"
+                j=Math.max(j,hm.get(c)+1);
+            }
+            hm.put(c,i);
+            maxLen=Math.max(maxLen,-j+i+1);
+        }
+         return maxLen;
+    }
+}
+
+//Approach2 using hashset
+
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
        HashMap<Character,Integer> hm=new HashMap<>();
